0e23c3495dca7e9522898b95dfadcc4c29cf373e
Subhankhalid1 <54278253+Subhankhalid1@users.noreply.github.com>
2020-06-11 06:53:55 +0500

    very first in class3

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Mytest from './testing';
+function App() {
+  
+  return (<div>
+    
+    <Mytest name="Project" uname="Subhan"/>
+</div>
+  )
+}
+
+export default App;
---
+++ testing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Mytest(props){
+    return<div>
+     <h1>   welcome to my first React {props.name} </h1>
+     <h4>   Made By  {props.uname} </h4>
+     <hr></hr>
+     <h1>   welcome to my first React {props.name} </h1>
+     <h4>   Made By  {props.uname} </h4>
+     <hr></hr>
+     <h1>   welcome to my first React {props.name} </h1>
+     <h4>   Made By  {props.uname} </h4>
+    </div>
+}
+export default Mytest;
